4e23c89336bfd114d3923a6475e7aa1787a9e807
priyansh-upadhyay <priyansh.manglam@gmail.com>
2025-10-06 11:27:17 +0530

    Day1.2: Second Largest

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
-public class LargestElementInArray{
-    public static int Largest(int[] arr){
-        int largest = arr[0];
-        for(int i =1;i<arr.length;i++){
+
+public class SecondLargest {
+    public static int find(int[] arr){
+        int largest =Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
             if(arr[i]>largest){
+                second = largest;
                 largest = arr[i];
             }
-
-        }return largest;
+        }
+        return second;
     }
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in Array: ");
         int n = sc.nextInt();
         System.out.println("Enter the Elements of Array: ");
@@ -19,8 +22,9 @@ public class LargestElementInArray{
             arr[i] = sc.nextInt();
         }
         
-        System.out.println("Largest element is: " + Largest(arr));
+        System.out.println("Second Largest element is: " + find(arr));
         sc.close();
     }
+    }
     
-}
+
