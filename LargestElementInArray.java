10841f79e08c8e8fe541b3729de173053ed4e843
Priyansh <priyansh.manglam@gmail.com>
2025-10-10 12:42:26 +0530

    Day 1:Largest element in an array

--- LargestElementInArray.java
+++ LargestElementInArray.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
-public class LargestElementInArray{
-    public static int Largest(int[] arr){
+
+public class LargestElementInArray {
+    public static int Largest(int[] arr) {
         int largest = arr[0];
-        for(int i =1;i<arr.length;i++){
-            if(arr[i]>largest){
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
                 largest = arr[i];
             }
-
-        }return largest;
+        }
+        return largest;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in Array: ");
         int n = sc.nextInt();
         System.out.println("Enter the Elements of Array: ");
         int[] arr = new int[n];
-        for(int i =0;i<n;i++){
-            arr[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Largest element is: " + Largest(arr));
+            sc.close();
         }
         
-        System.out.println("Largest element is: " + Largest(arr));
-        sc.close();
     }
-    
-}
+
