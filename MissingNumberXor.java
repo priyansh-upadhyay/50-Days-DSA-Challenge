dbeee889c263793eadb361f435bce2b1df1f95c3
Priyansh <priyansh.manglam@gmail.com>
2025-10-14 11:10:38 +0530

    day-5.2: missing number in array(xor)

---
+++ MissingNumberXor.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class MissingNumberXor {
+    public static int MissingNumber(int[] arr , int n){
+        int xor1 = 0;
+        int xor2 =0;
+        for(int i=1;i<=n;i++){
+             xor1 ^= i;
+        }
+        for(int num : arr){
+            xor2 ^= num;
+        }
+        return xor1 ^= xor2;
+    }
+public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter size of array:");
+    int n = sc.nextInt();
+
+    int[] arr = new int[n-1];
+
+    System.out.println("Enter the  " + (n-1)+" element of array:");
+    for(int i=0;i<n-1;i++){
+      arr[i] = sc.nextInt();
+    }
+   int missing = MissingNumber(arr ,n);
+    System.out.print("The missing number is : " + missing );
+
+    sc.close();
+}
+    
+}
