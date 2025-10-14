8f1e018a58416dadb4a077d451ef3bba9ff9c160
Priyansh <priyansh.manglam@gmail.com>
2025-10-14 10:45:08 +0530

    Find missing number(sum formula)

--- LeftRotateByOne.java
+++ LeftRotateByOne.java
@@ -32,7 +32,7 @@ public abstract class LeftRotateByOne {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        sc.close();
+        
     }
     
 }
