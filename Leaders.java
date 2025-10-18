54fb096b117757977eec58913512e8c76de9ab01
Priyansh <priyansh.manglam@gmail.com>
2025-10-18 21:26:45 +0530

    day-8:Leaders in array( bruteforce approch)

---
+++ Leaders.java
@@ -0,0 +1,21 @@
+public class Leaders {
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            boolean isLeader = true;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    isLeader = false;
+                    break;
+                }
+            }
+            if (isLeader)
+                System.out.print(arr[i] + " ");
+        }
+    }
+}
+ {
+    
+}
