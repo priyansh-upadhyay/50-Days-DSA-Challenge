7a0217c749d75bcdd9c23a98f1ad68dc782371ee
Priyansh <priyansh.manglam@gmail.com>
2025-10-29 22:06:39 +0530

    day -15 : Dutch national flag algorithm

---
+++ Arrays.java/DutchNationalFlag.java
@@ -0,0 +1,35 @@
+public class DutchNationalFlag {
+
+    public static void sortColors(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    int temp0 = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp0;
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    int temp2 = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp2;
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 0, 1, 2, 1, 0};
+        sortColors(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
--- String.java/StockProfit.java
+++ String.java/StockProfit.java
@@ -1,16 +1,16 @@
 public class StockProfit {
     public static int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
+        int maxProfitValue = 0;
 
         for (int price : prices) {
             if (price < minPrice)
                 minPrice = price;  
-            else if (price - minPrice > maxProfit)
-                maxProfit = price - minPrice;  
+            else if (price - minPrice > maxProfitValue)
+                maxProfitValue = price - minPrice;  
         }
 
-        return maxProfit;
+        return maxProfitValue;
     }
 
     public static void main(String[] args) {
