81d6eb787b1eaf4d716bae8bcd481fe9a4ce4508
Priyansh <priyansh.manglam@gmail.com>
2025-10-30 20:27:11 +0530

    day - 16 Merge sorted array

---
+++ Arrays.java/MergeSortedArray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class MergeSortedArray {
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int[] merged = new int[n1 + n2];
+        
+        int i = 0, j = 0, k = 0;
+        
+        // Merge elements in sorted order
+        while (i < n1 && j < n2) {
+            if (arr1[i] < arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+        
+        // Copy remaining elements
+        while (i < n1) merged[k++] = arr1[i++];
+        while (j < n2) merged[k++] = arr2[j++];
+        
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+        int[] merged = mergeArrays(arr1, arr2);
+
+        System.out.println(Arrays.toString(merged));
+    }
+}
