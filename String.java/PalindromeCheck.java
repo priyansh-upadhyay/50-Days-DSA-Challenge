66eff85d2c54cd8211ccaa8d71650c24dad4bbcd
Priyansh <priyansh.manglam@gmail.com>
2025-10-31 21:45:48 +0530

    day-16 palindrome string

---
+++ String.java/PalindromeCheck.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        
+        if (isPalindrome(str))
+            System.out.println("The string is a palindrome.");
+        else
+            System.out.println("The string is not a palindrome.");
+    }
+
+    public static boolean isPalindrome(String str) {
+        str = str.toLowerCase().replaceAll("\\s+", ""); // ignore case and spaces
+        int left = 0, right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
