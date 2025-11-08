cdd352dc38e6696f43530e0aa5c7ec8620723367
Priyansh <priyansh.manglam@gmail.com>
2025-11-08 08:22:16 +0530

    day -21 Check valid parenthesis string

---
+++ String.java/ValidParentheses.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        System.out.println(isValid(s) ? "Valid String" : "Invalid String");
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '['))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
